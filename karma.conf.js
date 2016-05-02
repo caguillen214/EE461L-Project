21bb2a408dede2d111d9db63702ba4cd53a613ad
Ben Fu <bennycooly@gmail.com>
2016-05-02 16:04:59 -0500

    basic karma test

--- karma.conf.js
+++ karma.conf.js
@@ -15,9 +15,21 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
+            // bower components
+            'bower_components/jquery/dist/jquery.js',
             'bower_components/angular/angular.js',
+            'bower_components/bootstrap/dist/js/bootstrap.js',
+            'bower_components/angular-bootstrap/ui-bootstrap.js',
+            'bower_components/angular-route/angular-route.js',
             'bower_components/angular-mocks/angular-mocks.js',
+            // module components
             'app.js',
+            'components/directives/*.module.js',
+            'components/services/**/*.module.js',
+            'components/main/main.js',
+            'components/main/main-controller.js',
+            'components/services/**/*.js',
+            'components/directives/*.js',
             'tests/*.js'
         ],
 
--- tests/example_test.js
+++ tests/example_test.js
@@ -1,8 +1,20 @@
 /**
  * Created by Ben on 4/29/2016.
  */
-describe('example test', function() {
-    it('should be true', function() {
-        expect(true).toBe(false);
+describe('MainController', function() {
+    beforeEach(function() {
+        module('mdMaker');
     });
+    var $controller;
+    beforeEach(inject(function(_$controller_) {
+        $controller = _$controller_;
+    }));
+    describe('$scope.showImage', function() {
+        it('is false', function() {
+            var $scope = {};
+            var contoller = $controller('MainController', {$scope: $scope});
+            expect($scope.showImage).toBe(false);
+        });
+    });
+
 });
